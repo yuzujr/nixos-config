404091d75e5c95a47a5e2e21509329080bd11a43
yuzujr <15568103056@163.com>
2026-04-18 21:08:33 +0800

    update

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -107,7 +107,18 @@
         enable = true;
         extraPortals = [
             pkgs.xdg-desktop-portal-gnome
+            pkgs.xdg-desktop-portal-gtk
         ];
+        config.niri = {
+            default = [
+                "gnome"
+                "gtk"
+            ];
+            "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+            "org.freedesktop.impl.portal.Access" = [ "gtk" ];
+            "org.freedesktop.impl.portal.Notification" = [ "gtk" ];
+            "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
+        };
     };
 
     # Keep Plasma installed, but disable KDE's crash handler entirely.
